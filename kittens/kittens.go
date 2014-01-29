c16979127722e4dd70772547afbf6877a49d62ab
Chance Zibolski <chance.zibolski@gmail.com>
2014-01-29 14:39:48 -0800

    Initial commit

---
+++ kittens/kittens.go
@@ -0,0 +1,55 @@
+package kittens
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"appengine"
+	"appengine/blobstore"
+)
+
+func serveError(c appengine.Context, w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Header().Set("Content-Type", "text/plain")
+	io.WriteString(w, "Internal Server Error")
+	c.Errorf("%v", err)
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	uploadURL, err := blobstore.UploadURL(c, "/upload", nil)
+	if err != nil {
+		serveError(c, w, err)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	t := template.Must(template.ParseFiles("templates/base.html", "templates/index.html"))
+	t.ExecuteTemplate(w, "base", uploadURL)
+}
+
+func handleServe(w http.ResponseWriter, r *http.Request) {
+	blobstore.Send(w, appengine.BlobKey(r.FormValue("blobKey")))
+}
+
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	blobs, _, err := blobstore.ParseUpload(r)
+	if err != nil {
+		serveError(c, w, err)
+		return
+	}
+	file := blobs["file"]
+	if len(file) == 0 {
+		c.Errorf("no file uploaded")
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	http.Redirect(w, r, "/serve/?blobKey="+string(file[0].BlobKey), http.StatusFound)
+}
+
+func init() {
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/serve/", handleServe)
+	http.HandleFunc("/upload", handleUpload)
+}
